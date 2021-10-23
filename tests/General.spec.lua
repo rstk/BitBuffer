8b5249e95f640024d35d5364fc4cec48ccbd2b10
rstk <rstkdev@gmail.com>
2021-10-23 14:14:41 -0400

    Remote :Fits

--- tests/General.spec.lua
+++ tests/General.spec.lua
@@ -27,13 +27,4 @@ return function()
 
 		expect(buffer:ReadUInt(32)).to.be.equal(0)
 	end)
-
-	it("Fits", function()
-		local buffer = BitBuffer.new(45)
-		buffer:WriteUInt(7, math.pow(2, 7) - 1)
-
-		expect(buffer:Fits(38)).to.be.equal(true)
-		expect(buffer:Fits(1)).to.be.equal(true)
-		expect(buffer:Fits(39)).to.be.equal(false)
-	end)
 end
